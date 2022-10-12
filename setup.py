2609609158ff223e8bb0b33d40d30ce261bd86a5
Максимилиан <pavlovmaksimilian1@gmail.com>
2022-10-12 20:03:26 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Geosteering',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Maksimilian Pavlov, Georgy Peshkov',
+    author_email='Maksimilian.Pavlov@skoltech.ru',
+    description=''
+)
