c9180cfc86e8b17f597c94d9b66002df196ec642
Максимилиан <pavlovmaksimilian1@gmail.com>
2023-01-30 10:29:03 +0300

    uploader

--- streamlit_app/Streamlit.py
+++ streamlit_app/Streamlit.py
@@ -5,5 +5,11 @@ Created on Sun Jan 29 18:25:09 2023
 @author: Максимилиан
 """
 import streamlit as st
+import pickle
+st.write("Geosteering based on resistivity")
 
-st.write("Hello ,let's learn how to build a streamlit app together")
+uploaded_file = st.file_uploader(label = 'Upload resistivity cube. It must be a  ')
+
+if uploaded_file is not None:
+    file = pickle.load(uploaded_file)
+    st.write(f'type of the file {type(file)}')
