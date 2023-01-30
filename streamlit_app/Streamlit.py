5e68215d9e4ec51f856afb099590b34d13535237
Максимилиан <pavlovmaksimilian1@gmail.com>
2023-01-30 10:10:35 +0300

    Streamlit_app

---
+++ streamlit_app/Streamlit.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 18:25:09 2023
+
+@author: Максимилиан
+"""
+import streamlit as st
+
+st.write("Hello ,let's learn how to build a streamlit app together")
