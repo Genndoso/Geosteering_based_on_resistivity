ef79347f896cdff81b21b4160a17d906c6c1f7df
Максимилиан <pavlovmaksimilian1@gmail.com>
2023-02-01 17:43:20 +0300

    Old code

---
+++ streamlit_app/main_code/upload_file.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import os
+import pickle
+
+def save_uploadedfile(uploadedfile):
+    with open(os.path.join("streamlit_app/data/raw/" ,uploadedfile.name) ,"wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success("Saved File:{} to server".format(uploadedfile.name))
+
+
+def upload_selected_file(path):
+    with open(os.path.join(path),"rb") as f:
+        file = pickle.load(f)
+    st.success("Dataset {} is successfully loaded".format((path.split('/')[-1])))
+    return file
